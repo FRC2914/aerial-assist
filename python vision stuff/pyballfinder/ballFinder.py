209869f9d1290fc0700aaabcd45192bc15ce4b42
squilter <s.quilter@aol.com>
2014-01-18 13:09:54 -0500

    Supports multi-object detection.  Thresholded by area set in conf file.

--- python vision stuff/pyballfinder/ballFinder.py
+++ python vision stuff/pyballfinder/ballFinder.py
@@ -17,6 +17,8 @@ saturation_lower = int(config.get('pyballfinder','saturation_lower'))
 saturation_upper = int(config.get('pyballfinder','saturation_upper'))
 value_lower = int(config.get('pyballfinder','value_lower'))
 value_upper = int(config.get('pyballfinder','value_upper'))
+min_contour_area = int(config.get('pyballfinder','min_contour_area'))
+area_difference_to_area_for_circle_detect = int(config.get('pyballfinder','area_difference_to_area_for_circle_detect'))
 print type(exposure)
 camera.set(cv2.cv.CV_CAP_PROP_EXPOSURE,exposure)#time in milliseconds. 5 gives dark image. 100 gives bright image.
 while(1):
@@ -42,32 +44,23 @@ while(1):
     contours,hierarchy = cv2.findContours(tobecontourdetected,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
     
 #    find contour with maximum area and store it as best_contour
-    if(contours):  
-        max_area = 0
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            if area > max_area:
-                max_area = area
-                best_contour = cnt
-        
-        perimeter = cv2.arcLength(best_contour, True)
-        real_area=max_area
-        calculated_area=math.pow((perimeter/(2*math.pi)),2)*math.pi
-        
-        area_difference=abs(real_area-calculated_area)
-        if(real_area!=0):
-            area_difference_to_area=int(area_difference/real_area*10)
-#             print(area_difference_to_area)
     
+    for contour in contours:  
+        real_area = cv2.contourArea(contour)
+        if real_area > min_contour_area:
+            perimeter = cv2.arcLength(contour, True)
+            calculated_area=math.pow((perimeter/(2*math.pi)),2)*math.pi
+            area_difference=abs(real_area-calculated_area)
+            area_difference_to_area=int(area_difference/real_area*10)
+  
 #    find centroids (fancy word for center of mass) of best_contour and draw a red circle there
-        M = cv2.moments(best_contour)#an image moment is the weighted average of a blob
-        cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-        cv2.circle(capture,(cx,cy),5,(0,0,255),-1)        
-         
-        if(area_difference_to_area<15):
-            cv2.putText(capture,"Ball",(cx,cy),cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255))
-        else:
-            cv2.putText(capture,"Bumper",(cx,cy),cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255))         
+            M = cv2.moments(contour)#an image moment is the weighted average of a blob
+            cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+            cv2.circle(capture,(cx,cy),5,(0,0,255),-1)                 
+            if(area_difference_to_area<area_difference_to_area_for_circle_detect):
+                cv2.putText(capture,"Ball",(cx,cy),cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255))
+            else:
+                cv2.putText(capture,"Bumper",(cx,cy),cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255))         
              
 #    show our image during different stages of processing
     cv2.imshow('capture',capture) 
