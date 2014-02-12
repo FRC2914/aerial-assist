02e5bd04424da6cc43995b5917f7ff67aff80c98
squilter <s.quilter@aol.com>
2014-02-12 16:40:08 -0500

    shooter is fast like speedy gonzales
    fixed math and it runs really smoothly now.

--- python vision stuff/mshooting/shooter.py
+++ python vision stuff/mshooting/shooter.py
@@ -27,6 +27,7 @@ value_lower = int(config.get('shooter','value_lower'))
 value_upper = int(config.get('shooter','value_upper'))
 lower_avg = int(config.get('shooter','lower_avg'))
 upper_avg = int(config.get('shooter','upper_avg'))
+min_pixel_weight = int(config.get('shooter','min_pixel_weight'))
 
 camera = cv2.VideoCapture(0)
 camera.set(cv2.cv.CV_CAP_PROP_EXPOSURE,exposure) #time in milliseconds. 5 gives dark image. 100 gives bright image.
@@ -41,24 +42,21 @@ try:
         hsvcapture = cv2.cvtColor(capture,cv2.COLOR_BGR2HSV)        
         inrangepixels = cv2.inRange(hsvcapture,np.array((hue_lower,saturation_lower,value_lower)),np.array((hue_upper,saturation_upper,value_upper)))#in opencv, HSV is 0-180,0-255,0-255
         
-        #gonna try to make this more efficient now.  Lots of math in python is a bad idea.
-        sumofpixels = 0
-        pixels = 0
-        for x in range(0,height):
-            for y in range(0,width):
-                if inrangepixels[x][y]==255:
-                    pixels = pixels+1
-                    sumofpixels+=x
-        avgheight = sumofpixels/(pixels)
-        
-        if(sumofpixels<2000):
+        try:
+            #This averaging math is explained in ./math.png
+            row_averages = np.mean(inrangepixels, axis=1)
+            avgheight=int(np.average(range(0,height), weights=row_averages, axis=0))
+        except ZeroDivisionError:
             avgheight=0
+        if(np.sum(row_averages, axis=0)<min_pixel_weight):
+            avgheight=0            
+        
         cv2.line(capture,(0,avgheight),(width,avgheight),(255,0,0),5)
         cv2.imshow("capture",capture)
         cv2.imshow("inrangepixels",inrangepixels)
         cv2.waitKey(1)
         #raise Exception("")
-except Exception as e: 
+except KeyboardInterrupt as e: 
     print "done"
     print e
     cv2.destroyAllWindows()
